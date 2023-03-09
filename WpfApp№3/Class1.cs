73e1ca30ab92fb2ebee971c9a281ab66555a7893
ulyanaukolova <isip_u.a.ukolova@mpt.ru>
2023-03-09 20:35:37 +0300

    fff

---
+++ WpfApp№3/Class1.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace EveryDayNick
+{
+    internal class Note
+    {
+        public string name;
+        public string description;
+        public DateTime? date;
+
+        public Note(string Name, string Description, DateTime? Date)
+        {
+            name = Name;
+            description = Description;
+            date = Date;
+        }
+
+        public static void Serialize<T>(List<T> notes)
+        {
+            string json = JsonConvert.SerializeObject(notes);
+            File.WriteAllText("notesInfo.json", $"\r\n{json}");
+        }
+
+
+        public static List<T> Deserialization<T>()
+        {
+            string name= File.ReadAllText("name");
+            List<T> notes = JsonConvert.DeserializeObject<List<T>>(name);
+            return notes;
+        }
+
+
+    }
+}
--- WpfApp№3/MainWindow.xaml.cs
+++ WpfApp№3/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,19 +14,82 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfApp_3;
 
-namespace WpfApp_3
+namespace EveryDayNick
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        int subIndex;
+        string name;
+        string description;
+        DateTime? dateTime = DateTime.Now;
+        static List<Note> notes = new List<Note>();
+        static List<Note> notesOnDays = new List<Note>();
+
         public MainWindow()
         {
             InitializeComponent();
+            notes = Note.Deserialization<Note>();
+            Calendar.SelectedDate = DateTime.Today;
+        }
 
-            daretime.SelectedDate = DateTime.Now;
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            DateTime? date = Calendar.SelectedDate;
+            int a = 0;
+            foreach (var delNote in notes)
+            {
+                if (delNote.date == notesOnDays[subIndex].date & delNote.description == notesOnDays[subIndex].description & delNote.name == notesOnDays[subIndex].name)
+                {
+                    a = notes.IndexOf(delNote); break;
+                }
+            }
+            notes.RemoveAt(a);
+            notesOnDays.RemoveAt(subIndex);
+            notesDataView.Items.RemoveAt(subIndex);
+            notes.Add(new Note(Name_note.Text, Description.Text, date));
+            notesOnDays.Add(new Note(Name_note.Text, Description.Text, date));
+            notesDataView.Items.Add(Name_note.Text);
+            Note.Serialize(notes);
+            notes = Note.Deserialization<Note>();
+            Empty();
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            int a = 0;
+            foreach (var delNote in notes)
+            {
+                if (delNote.date == notesOnDays[subIndex].date & delNote.description == notesOnDays[subIndex].description & delNote.name == notesOnDays[subIndex].name)
+                {
+                    a = notes.IndexOf(delNote); break;
+                }
+            }
+            notes.RemoveAt(a);
+            notesOnDays.RemoveAt(subIndex);
+            notesDataView.Items.RemoveAt(subIndex);
+            Note.Serialize(notes);
+            notes = Note.Deserialization<Note>();
+            subIndex = 0;
+            Empty();
+        }
+
+        private void Create_Click(object sender, RoutedEventArgs e)
+        {
+            name = Name_note.Text;
+            description = Description.Text;
+            dateTime = Calendar.SelectedDate;
+            Note note = new Note(name, description, dateTime);
+            notes.Add(note);
+            Note.Serialize(notes);
+            Note.Deserialization<Note>();
+            notesDataView.Items.Add(note.name);
+            notesOnDays.Add(note);
+            Empty();
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
@@ -32,19 +97,67 @@ namespace WpfApp_3
 
         }
 
-        private void button1_Click(object sender, RoutedEventArgs e)
+        private void Calendar1_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            Save.IsEnabled = false;
+            Delete.IsEnabled = false;
+            subIndex = 0;
+            notesDataView.Items.Clear();
+            notesOnDays.Clear();
+            Empty();
+            foreach (var note in notes)
+            {
+                if (note.date == Calendar.SelectedDate)
+                {
+                    notesOnDays.Add(note);
+                }
+            }
+            CheckDate();
+            foreach (var note in notesOnDays)
+            {
+                notesDataView.Items.Add(note.name);
+            }
+        }
+        private void CheckDate()
+        {
+            if (Calendar.SelectedDate != null)
+            {
+                isEnabled(true);
+            }
+            else
+            {
+                isEnabled(false);
+            }
+        }
+        private void isEnabled(bool a)
+        {
+            Name_note.IsEnabled = a;
+            Description.IsEnabled = a;
         }
 
-        private void button2_Click(object sender, RoutedEventArgs e)
-        {
 
+        private void Empty()
+        {
+            Name_note.Text = string.Empty;
+            Description.Text = string.Empty;
         }
 
-        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void notesDataView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            isEnabled(true);
+            subIndex = notesDataView.Items.IndexOf(notesDataView.SelectedItem);
+            if (subIndex != -1)
+            {
+                Save.IsEnabled = true;
+                Delete.IsEnabled = true;
+                Name_note.Text = notesOnDays[subIndex].name;
+                Description.Text = notesOnDays[subIndex].description;
+            }
+            else if (subIndex == -1 || notesDataView.SelectedItem == null)
+            {
+                Save.IsEnabled = false;
+                Delete.IsEnabled = false;
+            }
         }
     }
 }
